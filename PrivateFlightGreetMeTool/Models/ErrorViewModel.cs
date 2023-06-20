697b75331283b06bf9bf2806d22cf20b490cd4ef
Manojvgl <manojvgl@gmail.com>
2023-06-20 20:39:31 +1200

    init

--- PrivateFlightGreetMeTool/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrivateFlightGreetMeTool.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
